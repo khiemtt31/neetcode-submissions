222efc978954b25788163760972eb787c467c316
Tran Trong Khiem <203594247+khiemtt31@users.noreply.github.com>
2026-05-05 14:40:39 +0700

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const numSet = new Set();
+
+        for (const num of nums) {
+            if (numSet.has(num)) {
+                return true;
+            }
+            numSet.add(num);
+        }
+
+        return false;
+    }
+}
